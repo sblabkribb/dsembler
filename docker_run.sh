ad8718c42657d2f606b10314c8d03057a3deecac
aputron <64962874+aputron@users.noreply.github.com>
2021-09-03 09:45:43 +0900

    Delete docker_run.sh

--- docker_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#docker run -d --rm --name dsembler haseong/dsembler:v0.2
-docker run -d --rm -v /mnt/beta/dockervolume/dsembler_output_script:/app/output_script \
-	           -v /mnt/beta/dockervolume/dsembler_output:/app/output \
-		   --publish 5000:5000 --name dsembler dsembler:v8
